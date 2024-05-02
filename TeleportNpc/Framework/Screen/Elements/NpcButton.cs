4857ccd762aa90220444adb7cf0f336a998e336d
Enaium <wyy-666@hotmail.com>
2024-05-02 19:01:58 +0800

    better Mugshot

--- TeleportNpc/Framework/Screen/Elements/NpcButton.cs
+++ TeleportNpc/Framework/Screen/Elements/NpcButton.cs
@@ -8,7 +8,7 @@ using StardewValley.Menus;
 
 namespace TeleportNpc.Framework.Screen.Elements;
 
-public class NpcButton : Element
+public class NpcButton : BaseButton
 {
     private NPC Npc;
 
@@ -19,14 +19,28 @@ public class NpcButton : Element
 
     public override void Render(SpriteBatch b, int x, int y)
     {
-        Hovered = Render2DUtils.IsHovered(Game1.getMouseX(), Game1.getMouseY(), x, y, Width, Height);
-
         Render2DUtils.DrawButton(b, x, y, Width, Height, Hovered ? Color.Wheat : Color.White);
-        FontUtils.DrawHvCentered(b, Title, x + Width / 2, y + Height / 2);
-        if (!(Npc is Pet || Npc is Horse))
+        FontUtils.DrawHvCentered(b, Title, x, y, Width, Height);
+        if (Npc is not (Pet or Horse or TrashBear))
         {
             new ClickableTextureComponent("Mugshot", new Rectangle(x, y, Height, Height), "", "", Npc.Sprite.Texture,
                 Npc.getMugShotSourceRect(), 0.7f * Game1.pixelZoom).draw(b);
+        } else switch (Npc)
+        {
+            case Pet pet:
+                new ClickableTextureComponent("Mugshot", new Rectangle(x, y, Height, Height), "", "", Npc.Sprite.Texture,
+                    new Rectangle(0, pet.Sprite.SourceRect.Height * 2 - 24, pet.Sprite.SourceRect.Width, 24), 0.7f * Game1.pixelZoom).draw(b);
+                break;
+            case Horse horse:
+                new ClickableTextureComponent("Mugshot", new Rectangle(x, y, Height, Height), "", "", Npc.Sprite.Texture,
+                    new Rectangle(0, horse.Sprite.SourceRect.Height * 2 - 26, horse.Sprite.SourceRect.Width, 24), 0.7f * Game1.pixelZoom).draw(b);
+                break;
+            case TrashBear trashBear:
+                new ClickableTextureComponent("Mugshot", new Rectangle(x, y, Height, Height), "", "", Npc.Sprite.Texture,
+                    new Rectangle(0, trashBear.Sprite.SourceRect.Height, trashBear.Sprite.SourceRect.Width, 24), 0.7f * Game1.pixelZoom).draw(b);
+                break;
         }
+
+        base.Render(b, x, y);
     }
 }
