4a14254be376019b86ab5a8611b7f24c9b461442
Enaium <wyy-666@hotmail.com>
2024-05-16 01:23:19 +0800

    optimize the button for drawing mugshots

--- TeleportNpc/Framework/Screen/Elements/NpcButton.cs
+++ TeleportNpc/Framework/Screen/Elements/NpcButton.cs
@@ -1,5 +1,5 @@
-﻿using EnaiumToolKit.Framework.Screen.Elements;
-using EnaiumToolKit.Framework.Utils;
+﻿using EnaiumToolKit.Framework.Extensions;
+using EnaiumToolKit.Framework.Screen.Elements;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
@@ -10,35 +10,49 @@ namespace TeleportNpc.Framework.Screen.Elements;
 
 public class NpcButton : BaseButton
 {
-    private NPC Npc;
+    private readonly ClickableTextureComponent? _component;
 
     public NpcButton(string title, string description, NPC npc) : base(title, description)
     {
-        Npc = npc;
+        if (npc is not (Pet or Horse or TrashBear or Raccoon))
+        {
+            _component = new ClickableTextureComponent("Mugshot", default, "", "", npc.Sprite.Texture,
+                npc.getMugShotSourceRect(), 0.7f * Game1.pixelZoom);
+        }
+        else
+        {
+            _component = npc switch
+            {
+                Pet pet => new ClickableTextureComponent("Mugshot", default, "", "",
+                    npc.Sprite.Texture,
+                    new Rectangle(0, pet.Sprite.SourceRect.Height * 2 - 26, pet.Sprite.SourceRect.Width, 24),
+                    0.7f * Game1.pixelZoom),
+                Horse horse => new ClickableTextureComponent("Mugshot", default, "", "",
+                    npc.Sprite.Texture,
+                    new Rectangle(0, horse.Sprite.SourceRect.Height * 2 - 26, horse.Sprite.SourceRect.Width, 24),
+                    0.7f * Game1.pixelZoom),
+                TrashBear trashBear => new ClickableTextureComponent("Mugshot", default, "",
+                    "",
+                    npc.Sprite.Texture,
+                    new Rectangle(0, trashBear.Sprite.SourceRect.Height, trashBear.Sprite.SourceRect.Width, 24),
+                    0.7f * Game1.pixelZoom),
+                Raccoon raccoon => new ClickableTextureComponent("Mugshot", default, "", "",
+                    npc.Sprite.Texture,
+                    new Rectangle(0, raccoon.Sprite.SourceRect.Height * 2 - 26, raccoon.Sprite.SourceRect.Width, 24),
+                    0.7f * Game1.pixelZoom),
+                _ => null
+            };
+        }
     }
 
     public override void Render(SpriteBatch b, int x, int y)
     {
-        Render2DUtils.DrawButton(b, x, y, Width, Height, Hovered ? Color.Wheat : Color.White);
-        FontUtils.DrawHvCentered(b, Title, x, y, Width, Height);
-        if (Npc is not (Pet or Horse or TrashBear))
+        b.DrawButtonTexture(x, y, Width, Height, Hovered ? Color.Wheat : Color.White);
+        b.DrawStringCenter(Title!, x, y, Width, Height);
+        if (_component != null)
         {
-            new ClickableTextureComponent("Mugshot", new Rectangle(x, y, Height, Height), "", "", Npc.Sprite.Texture,
-                Npc.getMugShotSourceRect(), 0.7f * Game1.pixelZoom).draw(b);
-        } else switch (Npc)
-        {
-            case Pet pet:
-                new ClickableTextureComponent("Mugshot", new Rectangle(x, y, Height, Height), "", "", Npc.Sprite.Texture,
-                    new Rectangle(0, pet.Sprite.SourceRect.Height * 2 - 24, pet.Sprite.SourceRect.Width, 24), 0.7f * Game1.pixelZoom).draw(b);
-                break;
-            case Horse horse:
-                new ClickableTextureComponent("Mugshot", new Rectangle(x, y, Height, Height), "", "", Npc.Sprite.Texture,
-                    new Rectangle(0, horse.Sprite.SourceRect.Height * 2 - 26, horse.Sprite.SourceRect.Width, 24), 0.7f * Game1.pixelZoom).draw(b);
-                break;
-            case TrashBear trashBear:
-                new ClickableTextureComponent("Mugshot", new Rectangle(x, y, Height, Height), "", "", Npc.Sprite.Texture,
-                    new Rectangle(0, trashBear.Sprite.SourceRect.Height, trashBear.Sprite.SourceRect.Width, 24), 0.7f * Game1.pixelZoom).draw(b);
-                break;
+            _component.bounds = new Rectangle(x, y, Height, Height);
+            _component.draw(b);
         }
 
         base.Render(b, x, y);
